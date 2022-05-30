0ab23f129aed7911fb1a2a071c414f0d0d089b33
Annaati <Abdi17fatah@gmail.com>
2022-05-30 23:54:25 +0300

    EMS: Implemented Custom Validation(EMS.com domain only)

--- EmployeeManagement/EmployeeManagement/ViewModels/RegisterViewModel.cs
+++ EmployeeManagement/EmployeeManagement/ViewModels/RegisterViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EmployeeManagement.Utilities;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -12,6 +13,7 @@ namespace EmployeeManagement.ViewModels
         [Required]
         [EmailAddress]
         [Remote(action: "IsEmailInUse", controller: "Account")]
+        [ValidEmailDomain(allowedDomain: "EMS.com", ErrorMessage = "Email Domain must be EMS.com")]
         public String Email { get; set; }
 
         [Required]
