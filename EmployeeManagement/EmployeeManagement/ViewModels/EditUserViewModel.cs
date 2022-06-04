e5df492e34761c2fd5839c98ce88582871b0f0f9
Annaati <Abdi17fatah@gmail.com>
2022-06-05 00:21:12 +0300

    EMS: Implemented Edit Users ViewModel

---
+++ EmployeeManagement/EmployeeManagement/ViewModels/EditUserViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.ViewModels
+{
+    public class EditUserViewModel
+    {
+        public EditUserViewModel()
+        {
+            Claims = new List<string>();
+            Roles = new List<string>();
+        }
+
+        [Required]
+        public String Id { get; set; }
+
+        [Required]
+        public String UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public String Email { get; set; }
+
+        [Required]
+        public String FullName { get; set; }
+
+        [Required]
+        public String Gender { get; set; }
+
+        [Required]
+        public String Address { get; set; }
+
+
+        public List<String> Claims { get; set; }
+
+        public List<String> Roles { get; set; }
+    }
+}
